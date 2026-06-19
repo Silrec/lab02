257bc1a5cb864e06949313836bf7cea876f2702c
Silrec <451morgan.freeman@gmail.com>
2026-06-19 05:28:39 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 
+// Main entry point
 int main(int argc, char** argv)
 {
+    // Read user name from stdin
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
+    // Print greeting to stdout
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
